f4b762b878d6c041ed9d96b786c048416836d29d
Brandon Kase <bkase@cmu.edu>
2014-09-25 19:44:52 -0700

    Initial Commit

---
+++ gsoning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gsoning
+//
+//  Created by Highlight on 9/25/14.
+//  Copyright (c) 2014 Highlight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
