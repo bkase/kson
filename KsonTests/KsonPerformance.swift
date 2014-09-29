fc7e8e5f688a14447e3ab19560fa8449394de853
Brandon Kase <bkase@cmu.edu>
2014-09-28 23:19:24 -0700

    Set up framework for testing

---
+++ KsonTests/KsonPerformance.swift
@@ -0,0 +1,31 @@
+//
+//  KsonPerformance.swift
+//  KsonPerformance
+//
+//  Created by Highlight on 9/28/14.
+//  Copyright (c) 2014 Highlight. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class KsonPerformance: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measureBlock() {
+      // Put the code you want to measure the time of here.
+    }
+  }
+  
+}
--- KsonTests/KsonTests.swift
+++ KsonTests/KsonTests.swift
@@ -1,36 +1,20 @@
 //
 //  KsonTests.swift
-//  KsonTests
+//  Kson
 //
 //  Created by Highlight on 9/28/14.
 //  Copyright (c) 2014 Highlight. All rights reserved.
 //
 
 import UIKit
-import XCTest
+import Foundation
+import Nimble
+import Quick
 
-class KsonTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class KsonTests: QuickSpec {
+  override func spec() {
+    it("should") {
+      expect(1).to(equal(1))
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
 }
